425f073f3550cf513833ea4ec4f824f250255e9c
usuario <usuario@curso.es>
2020-03-31 15:48:42 +0200

    Tratamiento de texto con pandas y agrupaciones

--- FiltrarPalabras.py
+++ FiltrarPalabras.py
@@ -22,15 +22,18 @@ MI_MAESTRO = "local[2]"
 sesion = SparkSession.builder.master(MI_MAESTRO).appName("Contar Palabras").getOrCreate()
 # Crear RDD (lo hace la sesión): Paralelizamos la lista
 texto = [FunctionsTexto.leerfichero("Cuento.txt")]
-rdd= sesion.SparkContext.parallelize(texto)
+rdd= sesion.sparkContext.parallelize(texto)
 rdd1=rdd.flatMap(FunctionsTexto.contarpalabras)
 rdd2=rdd1.map(FunctionsTexto.limpiar_acentos)
 rdd3= rdd2.map(lambda palabra:palabra.upper())
 rdd4= rdd3.filter(FunctionsTexto.stop_words)
 rdd5=rdd4.map(lambda palabra: (palabra,1))
 rdd6=rdd5.reduceByKey(add)
-lista_puntuada=rdd6.collect()
+rdd7=rdd6.map(lambda dupla: (dupla[1],dupla[0]))
+rdd8=rdd7.sortByKey(False)
+rdd9=rdd8.map(lambda dupla: (dupla[1],dupla[0]))
+lista_puntuada=rdd9.collect()
 
 for(palabra,ocurrencias) in lista_puntuada:
-    print(palabra+"   "+str(ocurrencias))
+    print(str(palabra)+"   "+str(ocurrencias))
 sesion.stop()
